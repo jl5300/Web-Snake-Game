1e54324ad0b15551b995b8ba8759d8d52ed923a2
Joel Lozano <joel.lozano@columbia.edu>
2022-06-26 20:35:36 -0400

    Got server for webapp working with express

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 Simple Snake game written in Javascript, to be played in the browser. I decided to build this game to
 get some hands-on practice with basic web development.
+
+Sound effects downloaded from youtube.com.
--- snakeserver.js
+++ snakeserver.js
@@ -1,43 +1,10 @@
-const http = require('http');
+// const http = require('http');
+const path = require('path');
+const express = require('express');
 const fs = require('fs');
 
-const htmlPath = __dirname + '/snakegame.html';
-const scriptPath = __dirname + '/snakescript.js';
-const stylePath = __dirname + 'style.css';
+const app = express();
+const dir = path.join(__dirname, 'files');
 
-http.createServer((request, response) => {
-	fs.readFile(htmlPath, (data, err) => {
-		if (err) {
-			response.writeHead(404);
-			response.write(err);
-			response.end();
-		} else {
-			response.writeHead(200, {
-				'Content-Type': 'text/html'
-			});
-			response.write(data);
-		}
-	});
-	fs.readFile(stylePath, (data, err) => {
-		if (err) {
-			response.writeHead(400);
-			response.write(err);
-			response.end();
-		} else {
-			response.write(data);
-		}
-	});
-	fs.readFile(scriptPath, (data, err) => {
-		if (err) {
-			response.writeHead(400);
-			response.write(err);
-			response.end();
-		} else {
-			response.write(data);
-		}
-	});
-}).listen(8080);
-
-// app.get('/', (request, response) => {
-// 	response.sendFile(filePath);
-// });
+app.use(express.static(dir));
+app.listen(8080);
