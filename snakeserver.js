0b931dd338ade0912145d2e5d7c3193dce5c4389
Joel Lozano <joel.lozano@columbia.edu>
2022-11-25 20:26:09 -0500

    Moved logging into express.listen() callback

--- snakeserver.js
+++ snakeserver.js
@@ -5,6 +5,7 @@ const app = express();
 const dir = path.join(__dirname, 'files');
 
 app.use(express.static(dir));
-app.listen(8080);
 
-console.log('Server listening on port 8080. Go to localhost:8080/snakegame.html in any browser.');
+app.listen(8080, () => {
+	console.log('Server listening on port 8080. Go to localhost:8080/snakegame.html in any browser.');
+});
